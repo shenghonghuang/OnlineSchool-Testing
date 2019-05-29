25664d1b345e97eaea38e7943fb5e16557773c9f
Shenghong Huang <shenghong.huang@hotmail.com>
2019-05-29 13:21:50 +1200

    Completed login with valid account successfully

--- OnlineSchool/Program.cs
+++ OnlineSchool/Program.cs
@@ -1,3 +1,5 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +12,51 @@ namespace OnlineSchool
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Login_ValidInfo();
+        }
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        /* 1. login with valid user name and password, Remember me is selected
+           Expected Result(Assertion):login is successful, avatar is seen */
+        private static void Login_ValidInfo()
+        {
+            // Open
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://lyratesting2.co.nz/";
+            IWebElement webElement = null;
+            webElement = driver.FindElement(By.XPath("/html/body/div[1]/header/nav/div/ul/li[2]/a"));
+            webElement.Click();
+            // Username
+            webElement = driver.FindElement(By.Id("login_username"));
+            webElement.Click();
+            webElement.Clear();
+            webElement.SendKeys("test001");
+            // Password
+            webElement = driver.FindElement(By.Id("login_password"));
+            webElement.Click();
+            webElement.Clear();
+            webElement.SendKeys("Test1234");
+            // Checkbox
+            IWebElement ckRemeberMe = driver.FindElement(By.Name("_remember_me"));
+            Boolean vCheckedRemeberMe = false;
+            vCheckedRemeberMe = ckRemeberMe.Selected;
+            if (vCheckedRemeberMe == false)
+            {
+                webElement = driver.FindElement(By.Name("_remember_me"));
+                webElement.Click();
+            }
+            // Click Login
+            webElement = driver.FindElement(By.XPath("/html/body/div[1]/div[1]/div/div[2]/form/div[4]/button"));
+            webElement.Click();
+            // Check avatar
+            try
+            {
+                IWebElement imgAvatar = driver.FindElement(By.XPath("/html/body/div[1]/header/nav/div/ul/li[1]/a/img"));
+            }
+            catch (Exception e)
+            {
+            }
+            // Close
+            driver.Quit();
         }
     }
 }
