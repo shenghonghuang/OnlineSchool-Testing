70da474ea69642c3974ec77ba87795591d4bbccc
Shenghong Huang <shenghong.huang@hotmail.com>
2019-05-29 16:43:04 +1200

    Implemented Test with Specflow

---
+++ ClassLibrary_Specflow/Login.feature
@@ -0,0 +1,21 @@
+﻿Feature: Login
+	In order to learn online
+	As a member
+	I need to login in with my account
+
+@LoginSuccessful
+Scenario: LoginWithValidInfo
+	Given I am in home page
+	Then I click login
+	Then I input valid username and password, and make sure i have checked remember me
+	Then I can see the avatar on home page
+	Then I close the browser
+
+
+@LoginUnsuccessful
+Scenario: LoginWithInValidInfo
+	Given I am in home page
+	Then I click login
+	Then I input invalid username and password
+	Then I can see the error notification on login page
+	Then I close the browser
---
+++ ClassLibrary_Specflow/LoginSteps.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using ClassLibrary_PO;
+using TechTalk.SpecFlow;
+
+namespace ClassLibrary_Specflow
+{
+    [Binding]
+    public class LoginSteps
+    {
+        [Given(@"I am in home page")]
+        public void GivenIAmInHomePage()
+        {
+            Pages.HomePage.Goto();
+        }
+
+        [Then(@"I click login")]
+        public void ThenIClickLogin()
+        {
+            Pages.HomePage.ClickLogin();
+        }
+
+        [Then(@"I input valid username and password, and make sure i have checked remember me")]
+        public void ThenIInputValidUsernameAndPasswordAndMakeSureIHaveCheckedRememberMe()
+        {
+            Pages.LoginPage.LoginWithAccount("test001", "Test1234");
+        }
+
+        [Then(@"I can see the avatar on home page")]
+        public void ThenICanSeeTheAvatarOnHomePage()
+        {
+            Pages.HomePage.CheckAvatar();
+        }
+
+        [Then(@"I input invalid username and password")]
+        public void ThenIInputInvalidUsernameAndPassword()
+        {
+            Pages.LoginPage.LoginWithAccount("test", "Test");
+        }
+
+        [Then(@"I can see the error notification on login page")]
+        public void ThenICanSeeTheErrorNotificationOnLoginPage()
+        {
+            Pages.LoginPage.CheckErrorText();
+        }
+
+        [Then(@"I close the browser")]
+        public void ThenICloseTheBrowser()
+        {
+            Browser.Close();
+        }
+
+    }
+}
