15fc305430dc1c11eba02ea53c24e7292bb61548
Shenghong Huang <shenghong.huang@hotmail.com>
2019-05-30 14:38:19 +1200

    Change project structure

--- ClassLibrary_PO/SpecFlow/Login.feature.cs
+++ ClassLibrary_PO/SpecFlow/Login.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace ClassLibrary_Specflow
+namespace ClassLibrary.SpecFlow
 {
     using TechTalk.SpecFlow;
     
